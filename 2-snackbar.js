6684f1543bd584c0fdc6d60290202f1796cbba14
Vladislav220294 <86727727+Vladislav220294@users.noreply.github.com>
2025-01-12 16:28:22 +0000

    Deploying to gh-pages from @ Vladislav220294/goit-js-hw-101@b73b8128fec1c109b5176c653004eb99debdb8c8 🚀

--- 2-snackbar.js
+++ 2-snackbar.js
@@ -1,2 +1,2 @@
 import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as s}from"./assets/vendor-BbbuE1sJ.js";document.querySelector(".form").addEventListener("submit",t=>{t.preventDefault();const r=t.target.elements.delay.value,i=t.target.elements.state.value;((e,o)=>new Promise((m,l)=>{setTimeout(()=>{o==="fulfilled"?m(e):l(e)},e)}))(r,i).then(e=>{s.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{s.error({title:"Error",message:`❌ Rejected promise in ${e}ms`})})});
-//# sourceMappingURL=2-snacbar.js.map
+//# sourceMappingURL=2-snackbar.js.map
